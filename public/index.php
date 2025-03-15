3319a5a117d7fceb93826f228cac6012cf31907c
stevencclements <clements.steven@outlook.com>
2025-03-15 00:13:38 -0600

    Initial Commit | Application architecture
    Scaffolded initial application structure.

---
+++ public/index.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+define('BASE_PATH', dirname(__DIR__));
+
+require BASE_PATH . '/vendor/autoload.php';
+
+echo '<h1>Hello world!</h1>';
---
+++ trellis/Http/Stream.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * @package     Clementine\Trellis
+ * @author      Clementine Solutions
+ * @copyright   Clementine Technology Solutions LLC. (dba Clementine
+ *              Solutions). All rights reserved.
+ * 
+ * @version     1.0.0
+ * @since       1.0.0
+ */
+
+declare(strict_types=1);
+
+namespace Clementine\Trellis\Http;
+
+use Psr\Http\Message\StreamInterface;
+
+class Stream implements StreamInterface
+{
+    
+}
